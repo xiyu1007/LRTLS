ac4473245bdc6b65ba9e1454f476ec1a7db5cbb6
郭熙 <2983166962@qq.com>
2025-10-04 05:20:49 +0800

    Delete Requirement directory

--- Requirement/getDataInfo.m
+++
@@ -1,8 +0,0 @@
-function [n, c, M, d] = getDataInfo(X, Y) % 提取样本数n、类别数c、模态数M、特征维度d
-    M = numel(X);
-    [n, c] = size(Y);
-    d = zeros(1, M); % 预分配d的大小,1行m列
-    for m = 1:M
-        d(m) = size(X{m}, 2);% 遍历每个模态的样本矩阵X{m}，获取其列数，即第m个模态的特征维度
-    end
-end
